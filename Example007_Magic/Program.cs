91b7eb0a0a23bc5a80ede0c95b1daf4f3792aa8f
Maks <fonyak_maks@mail.ru>
2022-11-24 12:51:58 +0500

    Изменил код

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,9 +2,9 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 40, ya = 1,
+int xa = 30, ya = 1,
     xb = 1, yb = 20,
-    xc = 80, yc = 20;
+    xc = 60, yc = 20;
 
     Console.SetCursorPosition(xa, ya);
     Console.WriteLine("+");
@@ -19,20 +19,20 @@ int xa = 40, ya = 1,
 
     int count = 0;
 
-    while (count < 10);
+    while (count < 1000)
     {
         int what = new Random().Next(0, 3);
-        if (what == 0);
+        if (what == 0)
         {
             x = (x + xa) / 2;
             y = (y + ya) / 2;
         }
-        if (what == 1);
+        if (what == 1)
         {
             x = (x + xb) / 2;
             y = (y + yb) / 2;
         }
-        if (what == 2);
+        if (what == 2)
         {
             x = (x + xc) / 2;
             y = (y + yc) / 2;
